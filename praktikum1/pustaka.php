027931f845d8f7cf29941c121820a2d47237fc33
HIFZULT\Asus <kalawhzt@gmail.com>
2020-10-24 20:17:34 +0700

    Praktikum 1

---
+++ praktikum1/pustaka.php
@@ -0,0 +1,20 @@
+<?php
+    function aksiOperasi($a,$b,$o){
+        $n1 = $a;
+        $n2 = $b;
+
+        switch($o) {
+            case '+': {$hsl = $n1 + $n2; break;}
+            case '-': {$hsl = $n1 - $n2; break;}
+            case '/': {$hsl = $n1 / $n2; break;}
+            case 'x': {$hsl = $n1 * $n2; break;}
+        }
+        return $hsl;
+    }
+
+/*echo aksiOperasi(7,5,'x');
+echo '<br>';
+echo aksiOperasi(17,8,'/');
+*/
+
+?>
